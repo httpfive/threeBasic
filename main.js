bfa75804f1a568c6836db9bdcf4fc38396dbed70
httpfive <httpfive@gmail.com>
2020-07-25 00:42:55 +0300

    Scene setup

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 function init() {
     const scene = new THREE.Scene();
+
+    const box = getBox(1, 1, 1);
+    const plane = getPlane(4);
+    const pointLight = getPointLight(1);
+    const sphere = getSphere(0.04)
+
+    plane.name = 'plane-1'
+    box.name = 'box-1'
+    let enableFog = false;
+
+    if (enableFog) {
+        scene.fog = new THREE.FogExp2(0xfffff, 0.2)
+    }
+
+    scene.add(box);
+    scene.add(plane);
+    scene.add(pointLight);
+    pointLight.add(sphere)
+
+    box.position.y = box.geometry.parameters.height / 2;
+    plane.rotation.x = Math.PI / 2;
+
+
     const camera = new THREE.PerspectiveCamera(
         45,
         window.innerWidth / window.innerHeight,
@@ -7,14 +30,77 @@ function init() {
         1000
     );
 
+    camera.position.z = 5;
+    camera.position.x = 2;
+    camera.position.y = 10;
+    pointLight.position.y = 2;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor('rgb(0,0,0)')
     document.getElementById('webgl').appendChild(renderer.domElement);
+    update(renderer, scene, camera)
+    return scene;
+}
+
+function getPointLight(intensity) {
+    const light = new THREE.PointLight("rgb(255, 255, 255)", intensity);
+    return light
+
+}
+
+function getBox(w, h, d) {
+    const geometry = new THREE.BoxGeometry(w, h, d);
+    const material = new THREE.MeshPhongMaterial({
+        color: "rgb(255,255,255)"
+    });
+
+    const mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+    return mesh;
+}
+
+function getSphere(size) {
+    const geometry = new THREE.SphereGeometry(size, 24, 24);
+    const material = new THREE.MeshBasicMaterial({
+        color: "rgb(255,255,255)"
+    });
+
+    const mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+    return mesh;
+}
+
+
+function getPlane(size) {
+    const geometry = new THREE.PlaneGeometry(size, size);
+    const material = new THREE.MeshPhongMaterial({
+        color: "rgb(128,128,128)",
+        side: THREE.DoubleSide
+    });
+
+    const mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+    return mesh;
+}
+function update(renderer, scene, camera) {
     renderer.render(
         scene,
         camera
+    )
 
-    );
+    const box = scene.getObjectByName('box-1');
+    box.rotation.y += 0.001
+    requestAnimationFrame(function () {
+        update(renderer, scene, camera);
+    })
+
 }
-
-init();
+const scene = init();
